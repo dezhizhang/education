4017940fc95fc876948a3c757935cdf07fef3e5c
dezhizhang <1541609448@qq.com>
2020-02-08 22:15:32 +0800

    修改

--- app/router.js
+++ app/router.js
@@ -7,4 +7,6 @@ module.exports = app => {
   const { router, controller } = app;
   //router.get('/', controller.home.index);
   router.get("/",controller.default.home.index);
+  router.get("/index",controller.default.home.index);
+  router.get("/company",controller.default.company.index);
 };
